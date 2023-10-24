79e8cd6b5108face2f1dc94b1c47a5f506b2c795
Nikita_Nazarjev <mister.bro2013@yandex.ru>
2023-10-24 14:43:19 +0300

    Final version MyList

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,190 @@
 package org.example;
 
+
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 import java.util.function.Function;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+/**
+ * Для создания static inner class нужен объект родительского класса
+ * Для создания inner class достаточно указать родительский класс
+ * @param <T>
+ */
+
+public class MyList<T extends Number> implements Iterable<T>{
+
+  private final int START_LEN = 4;
+  public Object[] objArray = new Object[START_LEN];
+
+  private int lastIndex;
+
+  public MyList(){
+    lastIndex = 0;
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  public int hashCode(){
+    int result = lastIndex;
+    if(result>0){
+      for(Object d : objArray){
+        if(d instanceof Double){
+          int current = (int)((Double) d * 31);
+          result+=current;
+        }else if(d instanceof Float){
+          int current = (int)((Float) d * 31);
+          result+=current;
+        }else{
+          result+=(int)d;
+        }
+      }
+    }
+    return result;
   }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
+  @Override
+  public Iterator<T> iterator() {
+
+
+    return new Iterator<T>() {
+      int index = 0;
+      @Override
+      public boolean hasNext() {
+        return objArray[index] == null? false : true ;
+      }
+
+      @Override
+      public T next() {
+        return (T)objArray[index++];
+      }
+
+      @Override
+      public void remove() {
+            MyList.this.remove(index--);
+      }
+
+    };
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public String toString(){
+    String result = "";
+    for(int i = 0;i<lastIndex;i++){
+      result+=objArray[i] + " ";
+    }
+    return result.trim();
+  }
+
+  public static void main(String[] args) {
+    MyList <Integer> mlst = new MyList<>();
+    mlst.add(1);
+    mlst.add(2);
+    mlst.add(3);
+    Function f = new Function() {
+
+      public Object apply(Object o) {
+        return (Integer)o * 3;
+      }
+    };
+      MyList<Integer> copy = mlst.map(f);
+    System.out.println(copy.toString());
+    System.out.println("________________");
+
+  }
+
+
+  @Override
+  public boolean equals(Object obj) {
+    if(this == obj){return true;}
+    if(obj == null || this.getClass() != obj.getClass()){return false;}
+
+    MyList<T> lst = (MyList<T>) obj;
+    if(this.size()!=lst.size()){return false;}
+    if(this.size()!=0 && lst.size()!=0 && this.get(0).getClass()!= lst.get(0).getClass()) {return false;}
+    if(this.isEmpty()&&lst.isEmpty()){return false;}
+    for(int i = 0; i < this.size();i++){
+      // compare double
+      if(lst.get(i) instanceof Double||lst.get(i) instanceof Float){
+        double one = (Double) lst.get(i);
+        double two = (Double) this.get(i);
+        if(Double.compare(one,two)!=0){
+          return false;
+        }
+      }
+      //compare other
+      else{
+        int one = (int) lst.get(i);
+        int two = (int) this.get(i);
+        if(one!=two){
+          return false;}
+      }
+    }
+    return true;
+  }
+
+  public void add(T o) {
+       if(objArray.length == lastIndex){
+         resize();
+       }
+       objArray[lastIndex] = o;
+       lastIndex++;
   }
 
   public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
+    MyList<T> copy = new MyList<>();
+    for(int i = 0; i< lastIndex;i++){
+      copy.add((T) f.apply(objArray[i]));
+    }
+    return copy;
   }
 
-  public int size() {
-    throw new RuntimeException("Not implemented");
+
+
+
+  public T get(int index) {
+    if(index >= lastIndex || index < 0){
+      throw new IndexOutOfBoundsException();
+    }
+    return (T)objArray[index];
   }
+
+  private void resize() {
+    objArray = Arrays.copyOf(objArray, objArray.length + (objArray.length/2));
+  }
+
+  public void remove(int index) {
+    if(index >= lastIndex || index < 0){
+      throw new IndexOutOfBoundsException();
+    }
+    Object[] first = Arrays.copyOfRange(objArray,0,index);
+    Object[] second = Arrays.copyOfRange(objArray,index+1,objArray.length);
+    objArray = ArrayUtils.addAll(first, second);
+    lastIndex--;
+  }
+  public int size() {
+    return lastIndex;
+  }
+
+  public boolean isEmpty() {
+    if(lastIndex==0){return true;}
+         return false;
+  }
+
+  public boolean contains(Object o){
+    for(int i = 0;i < this.size();i++){
+      if(o.equals(this.get(i))){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public Object[] toArray() {
+    return Arrays.copyOf(objArray, lastIndex);
+  }
+
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -3,9 +3,124 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.function.Function;
+
 public class MyListTest {
   @Test
-  public void test() {
-    Assertions.assertTrue(true);
+  public void test_for_out_index() {
+    MyList<Integer> lst = new MyList<>();
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+    Assertions.assertThrows(IndexOutOfBoundsException.class,()-> lst.get(3));
+    Assertions.assertThrows(IndexOutOfBoundsException.class, ()->lst.get(-1));
   }
+
+  @Test
+  public void test_for_size_list(){
+    MyList<Short> sLst = new MyList<>();
+    Assertions.assertEquals(0,sLst.size());
+    MyList<Double> lst = new MyList<>();
+    lst.add(24.4);
+    lst.add(24.4);
+    Assertions.assertEquals(2,lst.size());
+  }
+
+  @Test
+  public void test_remove_myList(){
+    MyList<Long> lst = new MyList<>();
+    lst.add(12L);
+    lst.add(140L);
+    Assertions.assertEquals(2,lst.size());
+    lst.remove(0);
+    Assertions.assertEquals(140L,lst.get(0));
+    Assertions.assertEquals(1,lst.size());
+  }
+
+  @Test
+  public void test_empty(){
+    MyList<Float> lst = new MyList<>();
+    Assertions.assertTrue(lst.isEmpty());
+    lst.add(1.2F);
+    Assertions.assertFalse(lst.isEmpty());
+  }
+
+  @Test
+  public void test_hashCode(){
+    MyList<Double> lst1 = new MyList<>();
+    MyList<Double> lst2 = new MyList<>();
+    Assertions.assertEquals(lst1.hashCode(),lst2.hashCode());
+
+    lst1.add(22.4);
+    lst1.add(33.4);
+    lst1.add(44.72);
+
+    lst2.add(22.4);
+    lst2.add(33.4);
+    lst2.add(44.72);
+    Assertions.assertEquals(lst1.hashCode(),lst2.hashCode());
+  }
+
+  @Test
+  public void test_equals(){
+    MyList<Double> lst1 = new MyList<>();
+    MyList<Double> lst2 = new MyList<>();
+    MyList<Float>  lst3 = new MyList<>();
+    Assertions.assertFalse(lst1.equals(lst2));
+    lst1.add(12.3);
+    lst1.add(124.67);
+    lst1.add(12123.3234);
+    lst2.add(12.3);
+    lst2.add(124.67);
+    lst2.add(12123.3234);
+    lst3.add(12.3F);
+    lst3.add(124.67F);
+    lst3.add(12123.3234F);
+    Assertions.assertTrue(lst1.equals(lst2));
+    lst1.remove(0);
+    Assertions.assertFalse(lst1.equals(lst2));
+    Assertions.assertFalse(lst2.equals(lst3));
+  }
+
+  @Test
+  public void test_contains(){
+    MyList<Double> lst = new MyList<>();
+    lst.add(33.4);
+    lst.add(123.4);
+    Assertions.assertTrue(lst.contains(33.4));
+    Assertions.assertFalse(lst.contains(32.4));
+    Assertions.assertFalse(lst.contains(324));
+    Assertions.assertFalse(lst.contains(32.4F));
+  }
+
+  @Test
+  public void test_toString(){
+    MyList<Integer> lst = new MyList<>();
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+    String exected = "1 2 3";
+    Assertions.assertEquals(exected,lst.toString());
+  }
+
+  @Test
+  public void test_map(){
+
+    MyList<Integer> lst = new MyList<>();
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+    Function f = new Function() {
+      @Override
+      public Object apply(Object o) {
+        return (Integer) o * 2;
+      }
+    };
+    MyList<Integer> copy = lst.map(f);
+    String expected = "2 4 6";
+    Assertions.assertEquals(expected,copy.toString());
+  }
+
+
 }
